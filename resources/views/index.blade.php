f10877ea8d73c84e30d48db95030c5a3a041fa0f
michael <michaelrio9966@gmail.com>
2024-12-10 09:24:56 +0700

    admin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -111,7 +111,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link" style="background-color:#0998C1;">
+            <a href="{{route('home')}}" class="brand-link" style="background-color:#0998C1;">
                 <img src="{{asset('img/' . Auth::user()->company->logo)}}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-bold" style="color:white">{{Auth::user()->company->company_name}}</span>
