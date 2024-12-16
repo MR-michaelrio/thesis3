9a24f8db4635bca43e0ceb74b777278f4d579dc1
michael <michaelrio9966@gmail.com>
2024-12-16 10:03:46 +0700

    update

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -223,15 +223,18 @@ class EmployeeController extends Controller
                 // Only update password if provided
                 
 
-                if(Auth::user()->employee->id_employee != $employee->id_employee && $request->has('new_password')){
-                    if (!Hash::check($request->old_password, $user->password)) {
-                        return redirect()->back()->with('error', 'Old password is incorrect.');
-                    }
-                    // Update password
-                    $user->password = Hash::make($request->new_password);
-                }else{
+                if(Auth::user()->employee->id_employee != $employee->id_employee){
                     if ($request->has('password')) {
                         $user->password = Hash::make($request->input('password'));
+                    }
+                }else{
+                    
+                    if ($request->has('new_password')) {
+                        if (!Hash::check($request->old_password, $user->password)) {
+                            return redirect()->back()->with('error', 'Old password is incorrect.');
+                        }
+                        // Update password
+                        $user->password = Hash::make($request->new_password);
                     }
                 }
                 
