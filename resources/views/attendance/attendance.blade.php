14ac46134619415db6920b5d0c578811b876a674
michael <michaelrio9966@gmail.com>
2024-12-18 16:21:29 +0700

    update attendance

--- resources/views/attendance/attendance.blade.php
+++ resources/views/attendance/attendance.blade.php
@@ -186,12 +186,12 @@
                             return;
                         }else{
                             if (faceNames.length > 0) {
-                                document.getElementById('employeid').value = response.data.id_employee || ''; // Example key from response
+                                document.getElementById('employeid').value = response.data.full_name || ''; // Example key from response
                                 document.getElementById('employename').value = faceNames[0] || '';
                                 document.getElementById('clock').value = new Date().toLocaleTimeString(); // Current time
                                 document.getElementById('time').value = new Date().toLocaleTimeString();
 
-                                showPopup(faceNames[0],faceConfidence); // Show popup with the first detected name
+                                showPopup(esponse.data.full_name,faceConfidence); // Show popup with the first detected name
                             }
                         }
                     })
