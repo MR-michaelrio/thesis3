0b518858b6fa6d328b46f1909096ebadb2edf1f7
michael <michaelrio9966@gmail.com>
2024-12-18 17:15:45 +0700

    update attendance

--- resources/views/attendance/attendance.blade.php
+++ resources/views/attendance/attendance.blade.php
@@ -212,9 +212,8 @@
                                 console.log(faceid[0]);
                                 document.getElementById('employeid').value = response.data.employees[0].identification_number; // Example key from response
                                 document.getElementById('employename').value = response.data.employees[0].full_name;
-                                document.getElementById('clock').value = new Date().toLocaleTimeString(); // Current time
-                                document.getElementById('time').value = new Date().toLocaleTimeString();
-
+                                document.getElementById('clock').value = new Date().toLocaleTimeString([], { hour12: false });
+                                document.getElementById('time').value = new Date().toLocaleTimeString([], { hour12: false });
                                 showPopup(response.data.employees[0].full_name,faceid[0]); // Show popup with the first detected name
                             }
                         }
