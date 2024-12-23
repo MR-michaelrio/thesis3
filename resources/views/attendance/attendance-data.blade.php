117ba56c60d3d3c485da5dfad63106a68b27d390
michael <michaelrio9966@gmail.com>
2024-12-23 17:02:35 +0700

    update

--- resources/views/attendance/attendance-data.blade.php
+++ resources/views/attendance/attendance-data.blade.php
@@ -100,14 +100,16 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Name</td>
-                                    <td>ID</td>
-                                    <td>Department Code</td>
-                                    <td>Daily Total</td>
-                                    <td>Regular Hours</td>
-                                    <td>Overtime</td>
-                                </tr>
+                                @foreach($summary as $a)
+                                    <tr style="text-transform:capitalize;">
+                                        <td>{{$a->employee->full_name}}</td>
+                                        <td>{{$a->employee->user->identification_number}}</td>
+                                        <td>{{$a->employee->user->department->department_code}}</td>
+                                        <td style="font-weight:bold">{{ $a->daily_total ? \Carbon\Carbon::parse($a->daily_total)->format('H:i') . ' Hours' : '' }}</td>
+                                        <td>{{ \Carbon\Carbon::parse($a->shift->clock_in)->diff(\Carbon\Carbon::parse($a->shift->clock_out))->format('%H:%I') }} Hours</td>
+                                        <td style="color:red">{{$a->total_overtime}}</td>                                 
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
