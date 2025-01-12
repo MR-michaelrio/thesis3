51b02cb3ac81f64872e1a69d3ff0b8cdaf3deba2
michael <michaelrio9966@gmail.com>
2025-01-12 10:06:21 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -115,6 +115,8 @@
             <a href="{{route('home')}}" class="brand-link" style="background-color:#0998C1;">
                 @if(Auth::user()->role == "superadmin")   
                     <img src="{{ asset('assets/logo/logo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> 
+                    <span class="brand-text font-weight-bold" style="color:white">{{Auth::user()->company->company_name}}</span>
+
                 @else
                     <img src="{{asset('img/' . Auth::user()->company->logo)}}" alt="Logo"
                         class="brand-image img-circle elevation-3" style="opacity: .8">
